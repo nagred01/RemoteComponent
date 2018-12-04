87c0fa86ab443c3f0e1d1bf849d14862110f56a7
nagred01 <45577214+nagred01@users.noreply.github.com>
2018-12-04 13:29:22 -0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -8,18 +8,13 @@ Object.defineProperty(exports, "__esModule", {
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
   
 var _react = React;
-
 var _react2 = _interopRequireDefault(_react);
-
-  
-
-  
 var _reactNative = ReactNative;
    
-  return _react2.default.createElement(
+  
+return _react2.default.createElement(
           _reactNative.Text,
           { onPress : function () { alert(componentState.state.existingUserId) }},
           ['Nice Once Again. Thank you !!']
       );
-      
 })
