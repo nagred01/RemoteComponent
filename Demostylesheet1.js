e9e2b8a3b47f14947a559624cd830c976a9ff3ee
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-07 14:01:09 -0800

    Create Demostylesheet1.js

---
+++ Demostylesheet1.js
@@ -0,0 +1,44 @@
+(function main(StyleSheet) {
+    
+
+return StyleSheet.create({
+   container: {
+     flex: 1,
+     flexDirection: 'column',
+     justifyContent: 'center',
+     alignItems: 'center',
+     padding : 4
+  },
+  saveButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: 0.5,
+    borderWidth:0.1,
+    borderColor: "#0061b8", backgroundColor: "white",
+    marginTop:2
+  },
+  cancelButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: 0.5,
+    marginTop:6,
+    color: "white",
+    borderWidth: 0.1,
+    borderColor: "#015EBF", backgroundColor: "#0061b8"
+  },
+  padding: {
+    padding: 2,
+  },
+  textInput : {
+    borderWidth: 0.9, padding: 0, 
+    backgroundColor: "white", 
+    borderColor: "gray",
+    marginTop:1
+  },
+  labelMargin : {
+   marginTop:3,
+   color:"gray"
+  }
+});
+
+});
